68a20bf56b2d9c11440a658b67dbe645a1b770ee
Joshua Lemkin <joshua.lemkin@gmail.com>
2018-07-15 21:25:54 -0400

    chat bot

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators = [DataRequired()])
-    password = PasswordField('Password', validators = [DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('Usernadme', validators = [DataRequired()])
+    password = PasswordField('Pasadfsword', validators = [DataRequired()])
+    remember_me = BooleanField('Rememadfber Me')
+    ex = BooleanField('Example')
+    submit = SubmitField('dfd In')
